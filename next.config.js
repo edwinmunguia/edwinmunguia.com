ff186c11b1fd5d95b280cc32daf871f561efbb35
Edwin Munguia <edwinj.munguia90@gmail.com>
2024-02-22 20:54:01 -0600

    fix: :sparkles: allow images from 500px in next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drscdn.500px.org",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
