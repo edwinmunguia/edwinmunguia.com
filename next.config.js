570661a77fc4c9b0e491726069cf768b006d2e2e
Edwin J. Munguia <edwinj.munguia90@gmail.com>
2026-05-21 17:32:18 -0600

    feat: allow using images from unsplash

--- next.config.js
+++ next.config.js
@@ -9,6 +9,18 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
